d50de13f8eb1e22f6abd97da47faa80c65d96e83
Michal Wilczynski <michal.wilczynski@intel.com>
2023-05-22 17:21:20 +0300

    Remove redundant features - make extension more stable
    Signed-off-by: Michal Wilczynski <michal.wilczynski@intel.com>

--- src/extension.ts
+++ src/extension.ts
@@ -33,60 +33,6 @@ class CTagsDefinitionProvider implements vscode.DefinitionProvider {
   }
 }
 
-class CTagsHoverProvider implements vscode.HoverProvider {
-  public provideHover(
-    document: vscode.TextDocument,
-    position: vscode.Position,
-    token: vscode.CancellationToken
-  ): vscode.ProviderResult<vscode.Hover> {
-    const query = document.getText(document.getWordRangeAtPosition(position));
-    return this.resolveHover(query);
-  }
-
-  private async resolveHover(query: string): Promise<vscode.Hover | null> {
-    const matches = await tags.lookup(query);
-    if (!matches) {
-      util.log(`"${query}" has no matches.`);
-      return null;
-    }
-    util.log(`"${query}" has ${matches.length} matches.`);
-    const summary = matches.map(match => {
-      return (
-        path.relative(vscode.workspace.rootPath || '', match.path) +
-        ':' +
-        match.lineno
-      );
-    });
-    return new vscode.Hover(new vscode.MarkdownString(summary.join('  \n')));
-  }
-}
-
-class CTagsCompletionProvider implements vscode.CompletionItemProvider {
-  public provideCompletionItems(
-    document: vscode.TextDocument,
-    position: vscode.Position,
-    token: vscode.CancellationToken,
-    context: vscode.CompletionContext
-  ): vscode.ProviderResult<vscode.CompletionItem[] | vscode.CompletionList> {
-    const prefix = document.getText(document.getWordRangeAtPosition(position));
-    return this.resolveCompletion(prefix);
-  }
-
-  private async resolveCompletion(
-    prefix: string
-  ): Promise<vscode.CompletionItem[] | null> {
-    const matches = await tags.lookupCompletions(prefix);
-    if (!matches) {
-      util.log(`"${prefix}" has no matches.`);
-      return null;
-    }
-    util.log(`"${prefix}" has ${matches.length} matches.`);
-    return matches.map(match => {
-      return new vscode.CompletionItem(match.name);
-    });
-  }
-}
-
 function regenerateArgs(): string[] {
   const config = vscode.workspace.getConfiguration('ctags');
   const excludes = config
@@ -140,27 +86,6 @@ export function activate(context: vscode.ExtensionContext) {
   vscode.languages.registerDefinitionProvider(
     { scheme: 'file', language: 'c' },
     definitionsProvider
-  );
-
-  const hoverProvider = new CTagsHoverProvider();
-  vscode.languages.registerHoverProvider(
-    { scheme: 'file', language: 'c' },
-    hoverProvider
-  );
-  vscode.languages.registerHoverProvider(
-    { scheme: 'file', language: 'cpp' },
-    hoverProvider
-  );
-
-  const completionProvider = new CTagsCompletionProvider();
-  vscode.languages.registerCompletionItemProvider(
-    { scheme: 'file', language: 'c' },
-    completionProvider
-  );
-
-  vscode.languages.registerCompletionItemProvider(
-    { scheme: 'file', language: 'cpp' },
-    completionProvider
   );
 
   const regenerateCTagsCommand = vscode.commands.registerCommand(
